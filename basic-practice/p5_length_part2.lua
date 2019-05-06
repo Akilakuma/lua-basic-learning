8796c6c2984683d47e136387518ab30508a13a3d
akilakuma <akilakuma.su@gmail.com>
2019-05-06 16:49:25 +0800

    p5 再新增一個長度的判斷範例

---
+++ basic-practice/p5_length_part2.lua
@@ -0,0 +1,23 @@
+local arr = {}
+arr[2] = 2
+arr[3] = 3
+print(#arr) -- 0
+
+
+local arr1 = {}
+arr1[1] = 1
+arr1[2] = 2
+print(#arr1) -- 2
+
+
+local arr2 = {}
+arr2[2] = 2
+arr2[3] = 3
+arr2[4] = 4
+print(#arr2) -- 4
+
+
+local arr3 = {}
+arr3[1] = 1
+arr3[4] = 4
+print(#arr3) -- 1
